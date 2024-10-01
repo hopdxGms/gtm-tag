4e10d35d9c687a56df74c3d56738429365baeae0
dxhop <dxhop@gimasys.com>
2024-10-01 11:42:17 +0700

    update text

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ if (localKeyId == null) {
 }
 
 var label, len, countryCode = '';
-var post_data = JSON.parse(JSON.parse(document.getElementsByTagName('body').item(0).getAttribute('data-post')).portalFacts ? JSON.parse(document.getElementsByTagName('body').item(0).getAttribute('data-post')).portalFacts : '' ).forEach((item) => {
-    if (item.key === 'countryCode') {
-        countryCode = item.value
-    }
-    if (item.key === 'language') {
-        len = item.value
-    }
-});
+if (document.getElementsByTagName('body').item(0).getAttribute('data-post') && document.getElementsByTagName('body').item(0).getAttribute('data-post').portalFacts) {
+    JSON.parse(JSON.parse(document.getElementsByTagName('body').item(0).getAttribute('data-post').portalFacts)).forEach((item) => {
+        if (item.key === 'countryCode') {
+            countryCode = item.value;
+            if (localStorage.getItem('countryCode') == null) {
+                localStorage.setItem('countryCode', countryCode);
+            } else {
+                if (localStorage.getItem('countryCode') !== countryCode) {
+                    localStorage.setItem('countryCode', countryCode);
+                }
+            }
+        }
+        if (item.key === 'language') {
+            len = item.value
+        }
+    });
+}
 var language = document.getElementsByClassName("menu-title-underline")[0].innerHTML;
 link = `https://www.vietnamairlines.com/${countryCode}/${len.split('-')[0]}/legal/privacy-policy`;
 switch (language) {
@@ -21,7 +30,11 @@ switch (language) {
         label = "私は、プライバシーポリシーに詳述されているお知らせやニュースレター、プロモーション、ベトナム航空および当社のパートナーによる当社の製品・サービスに関するオファーなどのマーケティングコミュニケーションに同意します。";
         break;
     case "English":
-        label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.)`;
+        if (countryCode.toUpperCase() == 'VN' || localStorage.getItem('countryCode') == 'VN') {
+            label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.). Estimated frequency: 02 emails per week.`;
+        } else {
+            label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.)`;
+        }
         break;
     case "한국어":
         label = "개인 정보 보호 정책에 자세히 설명된 마케팅 커뮤니케이션(예: 알림, 뉴스레터, 프로모션, 베트남 항공 및 협력사 제품과 서비스에 관한 기타 제안 등)을 수락합니다";
@@ -45,8 +58,13 @@ switch (language) {
             "J'accepte les communications marketing telles que détaillées dans la politique de confidentialité (telles que les notifications, les bulletins d'information, les promotions, les autres offres en relation avec les produits et services de Vietnam Airlines et ses partenaires.)";
         break;
     default:
-        label =
+        if (countryCode.toUpperCase() == 'VN' || localStorage.getItem('countryCode').toUpperCase() == 'VN') {
+            label = `Tôi đồng ý nhận các thông tin quảng cáo, tiếp thị qua email được nêu chi tiết trong <a href="${link}">Chính sách bảo mật</a> (như thông báo, bản tin, khuyến mãi, các ưu đãi khác liên quan đến sản phẩm và dịch vụ của Vietnam Airlines và các đối tác của Vietnam Airlines). Dự kiến tần suất nhận khoảng 02 email/tuần.`
+        } else {
+            label =
             `Tôi đồng ý nhận các thông tin quảng cáo, tiếp thị qua email được nêu chi tiết trong <a href="${link}">Chính sách bảo mật</a> (như thông báo, bản tin, khuyến mãi, các ưu đãi khác liên quan đến sản phẩm và dịch vụ của Vietnam Airlines và các đối tác của Vietnam Airlines)`;
+        }
+        break;
 }
 var checkbox = document.createElement("mat-checkbox");
 checkbox.setAttribute("formcontrolname", "gdprConsent"),
@@ -68,7 +86,11 @@ if (null != appendingNode) {
                             break;
                         case "English":
                             len = 'en';
-                            label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.)`;
+                            if (countryCode.toUpperCase() == 'VN' || localStorage.getItem('countryCode') == 'VN') {
+                                label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.). Estimated frequency: 02 emails per week.`;
+                            } else {
+                                label = `I accept Marketing communications as detailed in the <a href="${link}"> Privacy Policy </a> (such as notifications, newsletters, promotions, other offers in connection with our product and services from Vietnam Airlines and our partners.)`;
+                            }
                             break;
                         case "한국어":
                             label = "개인 정보 보호 정책에 자세히 설명된 마케팅 커뮤니케이션(예: 알림, 뉴스레터, 프로모션, 베트남 항공 및 협력사 제품과 서비스에 관한 기타 제안 등)을 수락합니다";
@@ -99,7 +121,13 @@ if (null != appendingNode) {
                             break;
                         default:
                             len = 'vi'
-                            label = `Tôi đồng ý nhận các thông tin quảng cáo, tiếp thị qua email được nêu chi tiết trong <a href="${link}">Chính sách bảo mật</a> (như thông báo, bản tin, khuyến mãi, các ưu đãi khác liên quan đến sản phẩm và dịch vụ của Vietnam Airlines và các đối tác của Vietnam Airlines)`;
+                            if (countryCode.toUpperCase() == 'VN' || localStorage.getItem('countryCode').toUpperCase() == 'VN') {
+                                label = `Tôi đồng ý nhận các thông tin quảng cáo, tiếp thị qua email được nêu chi tiết trong <a href="${link}">Chính sách bảo mật</a> (như thông báo, bản tin, khuyến mãi, các ưu đãi khác liên quan đến sản phẩm và dịch vụ của Vietnam Airlines và các đối tác của Vietnam Airlines). Dự kiến tần suất nhận khoảng 02 email/tuần.`
+                            } else {
+                                label =
+                                `Tôi đồng ý nhận các thông tin quảng cáo, tiếp thị qua email được nêu chi tiết trong <a href="${link}">Chính sách bảo mật</a> (như thông báo, bản tin, khuyến mãi, các ưu đãi khác liên quan đến sản phẩm và dịch vụ của Vietnam Airlines và các đối tác của Vietnam Airlines)`;
+                            }
+                            break;
                     }
                     checkbox.innerHTML = `\n                    <div mat-internal-form-field="" class="mdc-form-field mat-internal-form-field">\n                            <div class="mdc-checkbox">\n                                <div class="mat-mdc-checkbox-touch-target">\n                                </div>\n                                <input \n                                    type="checkbox" \n                                    class="mdc-checkbox__native-control" \n                                    id="gdprConsentAds-input" tabindex="0" />\n                                <div class="mdc-checkbox__ripple"></div>\n                                <div class="mdc-checkbox__background">\n                                    <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" class="mdc-checkbox__checkmark">\n                                        <path fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" class="mdc-checkbox__checkmark-path"></path>\n                                    </svg>\n                                <div class="mdc-checkbox__mixedmark"></div>\n                            </div>\n                            <div mat-ripple="" class="mat-ripple mat-mdc-checkbox-ripple mat-mdc-focus-indicator"></div>\n                        </div>\n                        <label class="mdc-label" for="gdprConsentAds-input">\n                            <mat-label class="traveler-consent-label" for="gdprConsent">\n                                <span>\n                                    ${label}\n                                </span>\n                            </mat-label>\n                        </label>\n                        </div>`;
                 }
@@ -111,8 +139,7 @@ if (null != appendingNode) {
                 ? (checkbox.setAttribute("class", "mat-mdc-checkbox traveler-consent-checkbox ng-star-inserted ng-dirty ng-touched mat-accent mat-mdc-checkbox-checked ng-valid"),
                   myCheckbox.setAttribute("class", "mdc-checkbox__native-control mdc-checkbox--selected"))
                 : (checkbox.setAttribute("class", "mat-mdc-checkbox traveler-consent-checkbox ng-star-inserted ng-dirty ng-touched checkbox-error-state mat-warn ng-invalid"),
-                  myCheckbox.setAttribute("class", "mdc-checkbox__native-control")),
-                getToken();
+                  myCheckbox.setAttribute("class", "mdc-checkbox__native-control"));
         }),
         document.querySelector(".nextBtn.mdc-button.mdc-button--unelevated.mat-mdc-unelevated-button.mat-unthemed.mat-mdc-button-base.ng-star-inserted").addEventListener("click", async () => {
             var e = [],
@@ -143,7 +170,7 @@ if (null != appendingNode) {
                         PHONE: n,
                         "LAST-NAME": document.querySelector("input[id$='PersonalInfolastName']").value,
                         "FIRST-NAME": document.querySelector("input[id$='PersonalInfofirstName']").value,
-                        VENDOR: "1A",
+                        VENDOR: "GMS",
                         LANGUAGE: len,
                         COUNTRY: countryCode,
                         "DEVICE-ID": localKeyId,
